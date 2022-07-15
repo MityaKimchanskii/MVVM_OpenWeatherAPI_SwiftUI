c8293063ec1cc3b041be16d543f14f128bc1ba6d
Dmitrii Kim <mitya_kim@icloud.com>
2022-07-14 22:42:15 -0700

    Initial Commit

---
+++ MVVM_OpenWeatherAPI/MVVM_OpenWeatherAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVM_OpenWeatherAPIApp.swift
+//  MVVM_OpenWeatherAPI
+//
+//  Created by Mitya Kim on 7/14/22.
+//
+
+import SwiftUI
+
+@main
+struct MVVM_OpenWeatherAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
